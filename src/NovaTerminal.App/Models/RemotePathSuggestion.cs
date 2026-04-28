20c8aaec731f97817b772567b247230fe24d246f
benyblack <benyblack@gmail.com>
2026-04-28 18:31:57 +0200

    feat: add remote path autocomplete query primitives

---
+++ src/NovaTerminal.App/Models/RemotePathSuggestion.cs
@@ -0,0 +1,15 @@
+namespace NovaTerminal.Models;
+
+public sealed class RemotePathSuggestion
+{
+    public RemotePathSuggestion(string displayName, string fullPath, bool isDirectory)
+    {
+        DisplayName = displayName;
+        FullPath = fullPath;
+        IsDirectory = isDirectory;
+    }
+
+    public string DisplayName { get; }
+    public string FullPath { get; }
+    public bool IsDirectory { get; }
+}
